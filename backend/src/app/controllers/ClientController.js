b0e7680137731bd69e7277041779ac2793569305
Marcelo <marcelovqvd@gmail.com>
2020-01-08 16:26:17 -0300

    validation settings with Yup

--- backend/src/app/controllers/ClientController.js
+++ backend/src/app/controllers/ClientController.js
@@ -1,15 +1,34 @@
+import * as Yup from 'yup';
 import Client from '../models/ClientSchema';
 
 class ClientController {
+  async store(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      email: Yup.string()
+        .email()
+        .required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation failed' });
+    }
+
+    const clientExists = await Client.findOne({
+      where: { email: req.body.email },
+    });
+
+    if (clientExists) {
+      return res.status(400).json({ error: 'Client already exists' });
+    }
+
+    const { name, email } = await Client.create(req.body);
+    return res.json({ name, email });
+  }
+
   async index(req, res) {
     const name = await Client.find();
     return res.json(name);
-  }
-
-  async store(req, res) {
-    const client = await Client.create(req.body);
-
-    return res.json(client);
   }
 }
 
