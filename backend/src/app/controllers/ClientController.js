403d4fdec13ef4da439d48f9b71eff0c60232952
Marcelo <marcelovqvd@gmail.com>
2020-01-08 12:14:03 -0300

    index clients

--- backend/src/app/controllers/ClientController.js
+++ backend/src/app/controllers/ClientController.js
@@ -1,6 +1,11 @@
 import Client from '../models/ClientSchema';
 
 class ClientController {
+  async index(req, res) {
+    const name = await Client.find();
+    return res.json(name);
+  }
+
   async store(req, res) {
     const client = await Client.create(req.body);
 
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -3,6 +3,7 @@ import ClientController from './app/controllers/ClientController';
 
 const routes = new Router();
 
+routes.get('/clients', ClientController.index);
 routes.post('/newclient', ClientController.store);
 
 export default routes;
