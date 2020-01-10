b73a39b55ea41e51581f50109bf9e70b67d931d9
Marcelo <marcelovqvd@gmail.com>
2020-01-10 15:21:35 -0300

    clients list

--- frontend/src/pages/Main/index.js
+++ frontend/src/pages/Main/index.js
@@ -1,9 +1,34 @@
 import React, { Component } from 'react';
 
-export default class Main extends Component() {
+import api from '../../services/api';
+import { Lista } from './styles';
+
+export default class Main extends Component {
+  state = {
+    clients: [],
+  };
+
+  async componentDidMount() {
+    const response = await api.get(`/clients`);
+    console.log(response);
+    this.setState({
+      clients: response,
+    });
+  }
+
   render() {
+    const { clients } = this.state;
     return (
-        <h1>Main</h1>
+      <>
+        <Lista>
+          {clients.map(client => (
+            <li key={client.id}>
+              <p>{client.name}</p>
+              <p>{client.email}</p>
+            </li>
+          ))}
+        </Lista>
+      </>
     );
   }
 }
--- frontend/src/pages/Main/styles.js
+++ frontend/src/pages/Main/styles.js
@@ -1,1 +1,36 @@
 import styled from 'styled-components';
+
+export const Lista = styled.ul`
+  list-style: none;
+  li {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    margin-bottom: 60px;
+    a {
+    color: #fff;
+    text-decoration: none;
+    font-size: 16px;
+    padding: 20px;
+    margin: 10px;
+    width: 100px;
+    border: 1px solid black;
+    border-radius: 5px;
+    background-color: #000;
+    color: #fff;
+    }
+    a:hover {
+      opacity: 0.7;
+    }
+    p {
+    padding: 25px;
+    font-size: 26px;
+    }
+    img {
+      border: 0px solid black;
+      border-radius: 5px;
+      margin-bottom: 10px;
+    }
+  }
+`;
