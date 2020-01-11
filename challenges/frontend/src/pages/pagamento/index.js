8f7466d75272d093db2c647aeb78affa4785fb36
Gustavo Prizon <gustavoprizon@iMac-de-Gustavo.local>
2020-01-10 23:42:23 -0300

    criada a pag da forma de pagamento

--- challenges/frontend/src/pages/pagamento/index.js
+++ challenges/frontend/src/pages/pagamento/index.js
@@ -1,7 +1,43 @@
-import React from 'react';
+import React, { Component } from 'react';
+import api from '../../services/api';
 
-import { Container } from './styles';
+import Container from '../../components/container';
+import Footer from '../../components/footer';
+import { List } from './styles';
 
-export default function pagamento() {
-  return <div />;
+export default class Payment extends Component {
+  state = {
+    payments: [],
+  };
+
+  async componentDidMount() {
+    const response = await api.get(`/paymethod`);
+    console.log(response);
+    this.setState({
+      payments: response.data,
+    });
+  }
+
+  render () {
+    const { payments } = this.state;
+
+    return (
+      <Container>
+        <h1>Lista de Pagamentos</h1>
+
+        <List>
+        {payments.map(payment => (
+            <li key={payment.id}>
+              Nome do Cliente: <span>{payment.name_client}</span>
+              Method: <span>{payment.payment}</span>
+            </li>
+          ))}
+        </List>
+
+        <Footer>
+          <span>Desenvolvido por Gustavo Prizon</span>
+        </Footer>
+      </Container>
+    );
+  }
 }
--- challenges/frontend/src/pages/user/index.js
+++ challenges/frontend/src/pages/user/index.js
@@ -1,102 +1,35 @@
 import React, { Component } from 'react';
-import { FaPlus, FaSpinner } from 'react-icons/fa';
 import api from '../../services/api';
 
 import Container from '../../components/container';
 import Footer from '../../components/footer';
-import { Form, SubmitButton, List } from './styles';
+import { List } from './styles';
 
 export default class Home extends Component {
   state = {
-    newUser: '',
-    user: [],
-    loading: false,
-    error: null,
+    clients: [],
   };
 
-  // Carrega os dados do localStorage
-  componentDidMount() {
-    const users = localStorage.getItem('users');
-
-    if(users) {
-      this.setState({ users: JSON.parse(users) });
-    }
+  async componentDidMount() {
+    const response = await api.get(`/clients`);
+    console.log(response);
+    this.setState({
+      clients: response.data,
+    });
   }
-
-  // Salva os dados no localStorage
-  componentDidUpdate(_,prevSatet) {
-    const { users } = this.state;
-
-    if(prevSatet.users !== users){
-      localStorage.setItem('users', JSON.stringify(users));
-    }
-  }
-
-  handleInputChange = e => {
-    this.setState({ newUser: e.target.value, error: null });
-  };
-
-  handleSubmit = async e => {
-    e.preventDefault();
-
-    this.setState({ loading: true, error: false });
-
-    try {
-      const { newUser, users } = this.state;
-
-      if(newUser === '') throw 'Você precisa digitar um User';
-
-      const hasUser = users.find(r => r.name === newUser);
-
-      if(hasUser) throw 'User duplicado';
-
-      const response = await api.get(`/users/${newUser}`);
-
-      const data = {
-        name: response.data.name,
-        paymethod: response.data.forma_pagamento_id,
-      };
-
-      this.setState({
-        users: [...users, data],
-        newUser: '',
-      });
-    } catch (err) {
-      this.setState({ error: true });
-    } finally {
-      this.setState({ loading: false });
-    }
-  };
 
   render () {
-    const { newUser, users, loading, error } = this.state;
+    const { clients } = this.state;
 
     return (
       <Container>
-        <h1>Buscar Usuários!</h1>
-
-        <Form onSubmit={this.handleSubmit} error={error}>
-          <input
-            type="text"
-            placeholder="buscar usuário"
-            value={newUser}
-            onChange={this.handleInputChange}
-          />
-
-          <SubmitButton loading={loading}>
-            {loading ? (
-              <FaSpinner color="#FFF" size={14} />
-            ) : (
-              <FaPlus color="#FFF" size={14} />
-            )}
-          </SubmitButton>
-        </Form>
+        <h1>Lista de Usuários!</h1>
 
         <List>
-        {users.map(usuario => (
-            <li key={usuario.name}>
-              Name: <span>{usuario.name}</span>
-              Paymethod: <span>{usuario.paymethod}</span>
+        {clients.map(client => (
+            <li key={client.id}>
+              Name: <span>{client.name}</span>
+              Email: <span>{client.email}</span>
             </li>
           ))}
         </List>
